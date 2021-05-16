3ced864cb404eacad85afe2bd835eef1a944a164
Jakob Haugk <haugk.jakob@gmail.com>
2021-05-16 16:34:32 +0200

    1.6.2: prepared for https

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import * as path from 'path'
 import * as express from 'express'
 import * as cors from 'cors'
 import * as mongoose from 'mongoose';
+import * as fs from 'fs'
 import * as http from 'http'
 import * as https from 'https'
 
@@ -10,7 +11,7 @@ import router from './router';
 import { handleLogin } from './auth'
 import constants from './const'
 
-const { API_PORT, STATIC_PORT, HOST } = process.env
+const { HTTP_PORT, HTTPS_PORT, HOST, PRODUCTION } = process.env
 
 const { MONGO_URI } = process.env;
 const mongoOptions = {
@@ -23,33 +24,26 @@ const { imagePath, landingPagePath, adminPagePath, previewPagePath } = constants
 
 // static vue apps
 
-const staticApp = express();
+const app = express();
 
 // admin app
-staticApp.use('/admin', express.static(adminPagePath));
-staticApp.get('/admin', (req, res) => res.sendFile(path.join(adminPagePath, 'index.html')))
+app.use('/admin', express.static(adminPagePath));
+app.get('/admin', (req, res) => res.sendFile(path.join(adminPagePath, 'index.html')))
 
 // mirror of landing page app for preview purposes
-staticApp.use('/preview', express.static(previewPagePath));
-staticApp.get('/preview', (req, res) => res.sendFile(path.join(previewPagePath, 'index.html')))
+app.use('/preview', express.static(previewPagePath));
+app.get('/preview', (req, res) => res.sendFile(path.join(previewPagePath, 'index.html')))
 
 // landingpage app
-staticApp.use('/', express.static(landingPagePath));
-staticApp.get('/', (req, res) => res.sendFile(path.join(landingPagePath, 'index.html')))
-
-const staticServer = http.createServer(staticApp);
-staticServer.listen(STATIC_PORT, () => {
-  console.log(`static server running on ${HOST}:${STATIC_PORT}`)
-})
-
-
-
-// rest api app
-
-const app = express()
+app.use('/', express.static(landingPagePath));
+app.get('/', (req, res) => res.sendFile(path.join(landingPagePath, 'index.html')))
 
 // static images
 app.use('/images', express.static(imagePath))
+
+
+
+// rest api 
 
 app.use(express.json());
 app.use(cors())
@@ -58,14 +52,30 @@ app.use(cors())
 app.post('/login', handleLogin)
 app.use('/api', router)
 
-const apiServer = http.createServer(app)
+
+const httpServer = http.createServer(app)
+
 
 mongoose.connect(MONGO_URI, mongoOptions).then(() => {
 
-  apiServer.listen(API_PORT, async () => {
+  if (PRODUCTION === '1') {
 
-    console.log(`api server running on ${HOST}:${API_PORT}`)
-    
+    const credentials = {
+      key: fs.readFileSync(process.env.SSL_KEY, 'utf-8'),
+      cert: fs.readFileSync(process.env.SSL_CERT, 'utf-8'),
+      ca: fs.readFileSync(process.env.SSL_CA, 'utf-8'),
+    }
+  
+    const httpsServer = https.createServer(credentials, app)
+
+    httpsServer.listen(HTTPS_PORT || 443, () => {
+      console.log(`https server running on ${HOST}:${HTTPS_PORT}`)
+    })
+
+  }
+
+  httpServer.listen(HTTP_PORT || 80, () => {
+    console.log(`http server running on ${HOST}:${HTTP_PORT}`)
   })
 })
 
